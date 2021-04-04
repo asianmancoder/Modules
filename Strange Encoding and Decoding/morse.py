e0cecb618fd5722a2a4c1e313a9a08e3cb004623
joseph.L <josephdlee08@gmail.com>
2021-04-04 13:07:41 -0400

    Create morse.py

---
+++ Strange Encoding and Decoding/morse.py
@@ -0,0 +1,32 @@
+class MorseCode:
+
+  def __init__(self):
+    self.normToMorse = { 
+      'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', ' ':'/', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-', " ": " "
+                    }
+    
+    self.morse_decoded = {v:k for k, v in self.normToMorse.items()}
+
+  def encode(self, phrase):
+    return "".join([self.normToMorse[i] for i in phrase.upper()])
+  
+  def decode(self, phrase):
+    return "".join([self.morse_decoded[i] for i in phrase.upper().split('/')])
+
+
+mc = MorseCode()
+print(mc.encode('Joseph Lee'))
