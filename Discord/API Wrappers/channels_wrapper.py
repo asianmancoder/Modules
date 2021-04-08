33ac545332f6d53dbbb0bd299b5daf3f89300031
joseph.L <josephdlee08@gmail.com>
2021-04-08 16:55:07 -0400

    Update channels_wrapper.py

--- Discord/API Wrappers/channels_wrapper.py
+++ Discord/API Wrappers/channels_wrapper.py
@@ -1,1 +1,20 @@
+import requests
 
+class Channel:
+
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
+        self.payload = {}
+        self.headers = {}
+        self.json = {}
+        self.Authentication = None
+        self.base_url = "https://discord.com/api/v8/channels/"
+
+    def initialize_data(payload=None, headers=None, json=None, Authenication=None):
+        self.payload = payload
+        self.headers = headers
+        self.json = json
+        self.Authentication = Authentication
+    
+    def send_message(self, message):
+      pass
