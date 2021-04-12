8911b5c422efb39f2478d01350696824e6c64375
joseph.L <josephdlee08@gmail.com>
2021-04-11 20:57:47 -0400

    Update I_Commited.py

--- I_Commited.py
+++ I_Commited.py
@@ -1,4 +1,5 @@
 def I_Commited():
   print("oh, yes. i commited.")
 
-  # This was pushed to my Discord Server's channel: #tests
+# This was pushed to my Discord Server's channel: #github-updates
+# test
