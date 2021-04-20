ff5358f70a064f3351b3abc7ffcf40133ffb62a1
joseph.L <josephdlee08@gmail.com>
2021-04-19 20:15:42 -0400

    added random_trending_gif

--- misc/GiphyAPIWrapper.py
+++ misc/GiphyAPIWrapper.py
@@ -1,8 +1,12 @@
+# This is a Giphy API Wrapper for Trending, Search and Random.
+# When testing this module, use the example API Key: dc6zaTOxFJmzC
+# More functions coming soon.
+
 import requests
 import json
 import re
-# When testing, use this API Key: dc6zaTOxFJmzC
-# More functions coming soon!
+import random
+
 
 
 class Giphy:
@@ -12,14 +16,31 @@ class Giphy:
         self.api_key_param = "?api_key="
         self.API_KEY = API_KEY
 
-    def trending_gifs(self):
-        trending_url = f"{self.base_url}trending{self.api_key_param}{self.API_KEY}"
 
+    def random_trending_gif(self):
+        trending_url = f"{self.base_url}trending{self.api_key_param}{self.API_KEY}"
+        data = requests.get(trending_url).json()
+
+
+        with open("giphyData.txt", "w") as f:
+            f.write(str(data))
+
+        with open("giphyData.txt", "r") as f:
+            trending_json_data = f.readline()
+            giphyURLs = [x for x in re.findall("'url': '(.*?)'", trending_json_data)]
+
+            return random.choice(giphyURLs)
+
+
+    def first_trending_gif(self):
+        trending_url = f"{self.base_url}trending{self.api_key_param}{self.API_KEY}"
         data = requests.get(trending_url).json()["data"][0]["url"]
         return data
 
+
     def search_gifs(self, query):
         pass
+
 
     def random_gif(self):
         pass
