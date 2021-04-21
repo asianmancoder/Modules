25a297022ab4d45c01eba4685f36fdc85c201b05
joseph.L <josephdlee08@gmail.com>
2021-04-20 21:40:03 -0400

    AGHHHH
    Finally finished! See the note on line 59.

--- misc/Giphy/GiphyAPIWrapper.py
+++ misc/Giphy/GiphyAPIWrapper.py
@@ -1,12 +1,11 @@
-# This is a Giphy API Wrapper for Trending, Search and Random.
+# This is a Giphy API Wrapper for Trending, Search and Random with my added functions for more specific output.
 # When testing this module, use the example API Key: dc6zaTOxFJmzC
-# More functions coming soon.
 
 import requests
+from requests.utils import quote
 import json
 import re
 import random
-
 
 
 class Giphy:
@@ -18,29 +17,46 @@ class Giphy:
         self.trending_url = f"{self.base_url}trending?{self.api_key_param}{self.API_KEY}"
 
 
-    def random_trending_gif(self):
-        # There might be an issue with the efficiency of my code in this function; never fear I shall fix it later.
-        
+    # TRENDING GIF FUNCTIONS
+    def get_all_trending_gif_urls(self):
         data = requests.get(self.trending_url).json()
+        giphyTrendingURLs = [x for x in re.findall("'url': '(.*?)'", str(data)) if x[:34] != "https://giphy-analytics.giphy.com/"]
 
-        with open("giphyData.txt", "w") as f:
-            f.write(str(data))
+        return giphyTrendingURLs
+
+
+    def get_random_trending_gif_url(self):
+        return random.choice(self._all_trending_gif_urls())
+
+
+    def get_first_trending_gif_url(self):
+        return self.get_all_trending_gif_urls()[0]
+
+
+    # GIF SEARCH FUNCTIONS
+    def get_all_searched_gif_urls(self, query):
+        data = requests.get(f"{self.base_url}search?q={quote(query)}&{self.api_key_param}{self.API_KEY}").json()
+        giphySearchedURLs = [x for x in re.findall("'url': '(.*?)'", str(data))]
+
+        return giphySearchedURLs
+
+
+    def get_random_searched_gif_url(self, query):
+        return random.choice(self.get_all_searched_gif_urls(query))
+
+
+    def get_first_searched_gif_url(self, query):
+        return self.get_all_searched_gif_urls(query)[0]
+
+
+    # RANDOM GIF FUNCTIONS
+    def get_all_random_gif_urls(self, query):
+        data = requests.get(f"{self.base_url}random?{self.api_key_param}{self.API_KEY}&tag={quote(query)}").json()
+        giphyRandomSearchedURLs = [x for x in re.findall("'url': '(.*?)'", str(data))]
         
-        with open("giphyData.txt", "r") as f:
-            trending_json_data = f.readline()
-            giphyURLs = [x for x in re.findall("'url': '(.*?)'", trending_json_data)]
+        return giphyRandomSearchedURLs
 
-            return random.choice(giphyURLs)
-
-
-    def first_trending_gif(self):
-        data = requests.get(self.trending_url).json()["data"][0]["url"]
-        return data
-
-
-    def search_gifs(self, query):
-        pass
-
-
-    def random_gif(self):
-        pass
+    # I think there is some issue with this function... comapre it against the one above. If anyone has any suggestions on how to fix this, then my ears are stretched wide open. SHOUT IT OUT. EMAIL ME. DM ME ON DISCRD. PLEEEEEAAASSEEEEEEEEEE.
+    def get_first_random_gif_url(self, query):
+        return self.get_all_random_gif_urls(query)[0]
+    
