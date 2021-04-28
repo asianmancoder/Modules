f44f8d9c86f11926e6e1249fb172ce8f0e99b7d6
joseph.L <josephdlee08@gmail.com>
2021-04-28 19:09:47 -0400

    Update and rename nekoloveVersionOne_APIWRAPPER.py to nekolove_APIWRAPPER.py

---
+++ misc/nekolove_APIWRAPPER.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def nekoGet(endpoint):
+    neko_endpoints = ["neko", "nekolewd", "kitsune", "pat", "hug", "waifu", "cry", "kiss", "slap", "smug", "punch"]
+
+    if endpoint not in neko_endpoints:
+        return "Endpoint not found."
+        
+    data = requests.get(f"https://neko-love.xyz/api/v1/{endpoint}").json()['url']
+
+    return data
