3cb96b36e62e6ee87a29d44f9c4f08b082d93dd2
joseph.L <josephdlee08@gmail.com>
2021-05-04 16:00:26 -0400

    It is finished!

--- Network thingies/Sockets/chat_server.py
+++ Network thingies/Sockets/chat_server.py
@@ -20,13 +20,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
 
         try:
             while connected:
-                data = conn.recv(2048)
+                data = conn.recv(2048).decode('utf-8')
                 print(f"{addr}: {data}")
 
                 if not data:
                     break
 
-                d = bytes(input("Type a message here: "), 'utf-8')
+                d = input("Type a message here: ").encode('utf-8')
                 conn.sendall(d)
 
         except ConnectionResetError:
