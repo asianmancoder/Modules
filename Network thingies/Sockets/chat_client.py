67ff6f0828e999179e41548d57a1edf6480da373
joseph.L <josephdlee08@gmail.com>
2021-05-04 16:00:08 -0400

    yay

--- Network thingies/Sockets/chat_client.py
+++ Network thingies/Sockets/chat_client.py
@@ -12,10 +12,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
 
     try:
         while True:
-            d = bytes(input("Type a message here: "), 'utf-8')
+            d = input("Type a message here: ").encode('utf-8')
             client.sendall(d)
 
-            data = client.recv(2048)
+            data = client.recv(2048).decode('utf-8')
             print(f"{HOST}: {data}")
 
     except ConnectionResetError:
