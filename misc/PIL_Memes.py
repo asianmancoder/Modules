d738aaabec6b3d551ed5fea02b9484b2bc9b8654
joseph.L <josephdlee08@gmail.com>
2021-05-14 12:30:08 -0400

    There is still an issue with this...
    I can't seem to get truetype to work

---
+++ misc/PIL_Memes.py
@@ -0,0 +1,14 @@
+from io import BytesIO
+import requests
+from PIL import Image, ImageDraw, ImageFont
+
+
+def meme(text):
+    img_bytes = requests.get("https://i.imgflip.com/2xytmc.jpg").content
+    bIO_obj = BytesIO(img_bytes)
+
+    with Image.open(bIO_obj) as img:
+        width, height = img.size
+        draw = ImageDraw.Draw(img)
+        draw.text(((width/2) - 300, (height/2) + 300), text, fill=(0, 0, 0))
+        img.show()
