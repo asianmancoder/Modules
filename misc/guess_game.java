207043d4b13e78fb42231fadc99b3ed012117a41
joseph.L <josephdlee08@gmail.com>
2021-06-16 13:21:05 -0400

    Create guess_game.java
    This is a guessing game that I made when I was bored.

---
+++ misc/guess_game.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class guess_game {
+    public static void main(String[] args) {
+        int guess = 0;
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int randint = random.nextInt(101);
+
+        while(guess != randint) {
+            System.out.println("I'm thinking of a number from 0 to 100... what is your guess?");
+            guess = scanner.nextInt();
+
+            if(guess > randint && (guess - randint) < 10) {
+                System.out.println("You're very close... your guess is just a little more than the number!");
+            }
+            else if(guess < randint && (randint - guess) < 10) {
+                System.out.println("You're very close... your guess is just a little less than the number!");
+            }
+            else if(guess > 100 || guess < 0 && guess != -1) {
+                System.out.println("Your guess is off the charts. I don't mean that in a good way.");
+            }
+            else if(guess == randint) {
+                System.out.println("You got it correct!");
+                break;
+            }
+            else if(String.valueOf(guess) == "I give up") {
+                System.out.println("The number was: " + String.valueOf(guess));
+            }
+            else if(guess != randint && ((guess - randint) > 10 || (randint - guess) > 10)) {
+                System.out.println("No.");
+            }
+            else if(guess == -1) {
+                System.out.println("The number was: " + randint);
+            }
+        }
+    }
+}
