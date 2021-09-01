5c208f81ac5872ffaeb690864a1ec772e4f364c7
joseph.L <josephdlee08@gmail.com>
2021-09-01 13:17:31 -0400

    replaced crappy code with new, good code

--- misc/filters.py
+++ misc/filters.py
@@ -3,18 +3,54 @@ import json
 
 class Filters:
 
-  def __init__(self):
-    with open('filter_file.json', 'r') as f:
+  def __init__(self, guild_id):
+    with open("filter_file.json", "r") as f:
       self.filter_data = json.loads(f.read())
+    
+    self.guild_id = guild_id
 
   def save(self):
-    with open('filter_file.json', 'w') as f:
+    with open("filter_file.json", "w") as f:
       f.write(json.dumps(self.filter_data))
 
-  def filter_add(self, trigger, response):
-    self.filter_data[trigger] = response
+  def add_guild(self, guild_id):
+    self.filter_data[guild_id] = {}
     self.save()
-    return "Done"
 
-  def filter_do(self, trigger):
-    return self.filter_data[trigger]
+  def delete_guild(self, guild_id):
+    if not self.filter_data[guild_id]:
+      return f"Guild {guild_id} does not exist."
+    else:
+      del self.filter_data[guild_id]
+      self.save()
+
+  def add_filter(self, guild_id, trigger, response):
+    if not self.filter_data[guild_id]:
+      return f"Guild {guild_id} does not exist."
+    else:
+      self.filter_data[guild_id][trigger] = response
+      self.save()
+
+  def delete_filter(self, guild_id, trigger):
+    if not self.filter_data[guild_id][trigger]:
+      return f"Trigger {trigger} not found."
+    else:
+      del self.filter_data[guild_id][trigger]
+      self.save()
+  
+  def do_filter(self, guild_id, trigger):
+    if not self.filter_data[guild_id][trigger]:
+      return f"Trigger {trigger} not found."
+    else:
+      return self.filter_data[guild_id][trigger]
+
+  def contains(self, message, s):
+    if s in message:
+      return True
+    else:
+      return False
+
+  def check_message(self, message, guild_id):
+    for trigger in self.filter_data[guild_id]:
+      if self.contains(trigger, message):
+        return self.filter_data[guild_id][trigger]
